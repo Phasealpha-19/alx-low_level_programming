4f419b0ec5b054bef5a894f1d289921c9ea086c4
Olufemi Olagbaju <femiolagbaju123@gmail.com>
2022-09-19 16:58:01 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _puts - Entry point;
- * @str: string being used
- * Return: Always 0 Sucess
+ *_puts - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * On success: returns no error
  */
+
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
+	int i = 0;
 
-	_putchar('\n');
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
 }
