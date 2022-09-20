952cdf8d932a4099589bc8fd7019915403fac36c
Olufemi Olagbaju <femiolagbaju123@gmail.com>
2022-09-19 17:03:25 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * puts2 - Entry point
- * @str: character being used
- * Return: Always 0 Success
+ * puts2 - prints every other character
+ * @str: string
+ *
+ * Return: nothing
  */
+
 void puts2(char *str)
 {
-	int index = 0, len = 0;
+	int i = 0;
 
-	while (str[index++])
-		len++;
-
-	for (index = 0; index < len; index += 2)
-		_putchar(str[index]);
-
-	_putchar('\n');
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
 }
